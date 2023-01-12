eb2c09f484d29f178a78d40d23e12ebb4ac045f1
Yousef Almassad <yousef@Yousefs-MacBook-Pro-2.local>
2023-01-12 19:18:41 +0300

    Submit project 1
    Signed-off-by: Yousef Almassad <yousef@Yousefs-MacBook-Pro-2.local>

--- Project 1/src/Main.java
+++ Project 1/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     static Set<Integer> playedMoves = new HashSet<Integer>();
     static boolean isFinished = false;
     static boolean isWinner = false;
+    static String winner = "none";
     static String currentPlayer = "player";
     static int move = -1;
     static Scanner scanner = new Scanner(System.in);
@@ -27,8 +28,8 @@ public class Main {
             else
                 computerMove();
 
-
-            //Check if game finished
+            checkWinner();
+            isGameFinished();
         }
 
         // Show winner
@@ -41,7 +42,7 @@ public class Main {
 
         if (isValidMove(move)){
             playedMoves.add(move);
-            makeMove(move);
+            makeMove(move, 'X');
         } else
             System.out.println("Invalid move please make a move from (1-9) and not in an already played spot.");
 
@@ -54,16 +55,19 @@ public class Main {
         int randomMove = -1;
         while (!isValidMove){
             randomMove = 0 + random.nextInt(8 - 0 + 1);
-            if(!playedMoves.contains(randomMove))
-                isValidMove = true;
+            isValidMove = isValidMove(randomMove);
         }
         playedMoves.add(randomMove);
-        makeMove(randomMove);
+        System.out.println("The computer move index is: "+ randomMove);
+        makeMove(randomMove, 'O');
     }
 
-    static void makeMove(int place){
-        isValidMove(place);
+    static void makeMove(int place, char sympol){
 
+        updateBoard(place, sympol);
+        displayBoard();
+
+        currentPlayer = currentPlayer == "player" ? "computer" : "player";
     }
 
     static boolean isValidMove(int place){
@@ -72,11 +76,107 @@ public class Main {
         return false;
     }
 
+    static void checkWinner(){
+        if (checkWinnerIfAny() == "player") {
+            System.out.println("You win!");
+            isFinished = true;
+            isWinner = true;
+            winner = "player";
+        }
+        else if (checkWinnerIfAny() == "computer") {
+            System.out.println("Computer wins!");
+            isFinished = true;
+            isWinner = true;
+            winner = "computer";
+        }
+    }
+
+    static void isGameFinished(){
+        if (playedMoves.size() == 9){
+            isFinished = true;
+        }
+    }
+
     static void displayBoard(){
         for (char[] row: board){
             for (char character : row)
                 System.out.print(character);
             System.out.println();
         }
+    }
+
+    static void updateBoard(int place, char symbol){
+
+        switch (place){
+            case 0:
+                board[0][0] = symbol;
+                break;
+            case 1:
+                board[0][2] = symbol;
+                break;
+            case 2:
+                board[0][4] = symbol;
+                break;
+            case 3:
+                board[2][0] = symbol;
+                break;
+            case 4:
+                board[2][2] = symbol;
+                break;
+            case 5:
+                board[2][4] = symbol;
+                break;
+            case 6:
+                board[4][0] = symbol;
+                break;
+            case 7:
+                board[4][2] = symbol;
+                break;
+            case 8:
+                board[4][4] = symbol;
+                break;
+            default:
+                break;
+        }
+    }
+
+    static String checkWinnerIfAny(){
+
+        for (int i = 0; i < 8; i++) {
+            String lineSympols = "";
+            switch (i){
+                case 0:
+                    lineSympols = board[0][0] +""+ board[0][2] +""+ board[0][4];
+                    break;
+                case 1:
+                    lineSympols = board[2][0] +""+ board[2][2] +""+ board[2][4];
+                    break;
+                case 2:
+                    lineSympols = board[4][0] +""+ board[4][2] +""+ board[4][4];
+                    break;
+                case 3:
+                    lineSympols = board[0][0] +""+ board[2][0] +""+ board[4][0];
+                    break;
+                case 4:
+                    lineSympols = board[0][2] +""+ board[2][2] +""+ board[4][2];
+                    break;
+                case 5:
+                    lineSympols = board[0][4] +""+ board[2][4] +""+ board[4][4];
+                    break;
+                case 6:
+                    //Diagonal 1
+                    lineSympols = board[0][0] +""+ board[2][2] +""+ board[4][4];
+                    break;
+                case 7:
+                    //Diagonal 2
+                    lineSympols = board[0][4] +""+ board[2][2] +""+ board[4][0];
+                    break;
+            }
+            if (lineSympols.equals("XXX"))
+                return "player";
+            else if (lineSympols.equals("OOO"))
+                return "computer";
+        }
+        return "none";
     }
 }
