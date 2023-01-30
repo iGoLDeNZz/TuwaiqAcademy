fb32169cc6bc135d99763713a1348032e0ef6732
Yousef <yousef1415@hotmail.com>
2023-01-30 21:36:59 +0300

    Update constraint
    Signed-off-by: Yousef <yousef1415@hotmail.com>

--- Week 4/Day 2/src/main/java/com/example/day_2/Model/User.java
+++ Week 4/Day 2/src/main/java/com/example/day_2/Model/User.java
@@ -1,10 +1,7 @@
 package com.example.day_2.Model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,16 +15,16 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Integer id;
-    
+
 
     @NotEmpty(message = "Name cannot be empty")
-    @Min(value = 5, message = "Name must be longer than 4 characters.")
+    @Size(min = 5, message = "Name must be longer than 4 characters.")
     @Column(columnDefinition = "varchar(30) not null CHECK ( LENGTH(name)>4 )")
     private String name;
 
 
     @NotEmpty(message = "Username cannot be empty")
-    @Min(value = 5, message = "Username must be longer than 4 characters.")
+    @Size(min = 5, message = "username must be longer than 4 characters.")
     @Column(columnDefinition = "varchar(30) not null CHECK (LENGTH(username)>4) unique")
     private String username;
 
